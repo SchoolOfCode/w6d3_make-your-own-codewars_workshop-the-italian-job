8a1e373ff66b9f5b105abae4bbd85de385ecc411
valeriocipolla <valeriocipolla92@gmail.com>
2021-12-22 19:22:48 +0000

    kata ready for codewars, tests written

--- main.test.js
+++ main.test.js
@@ -1,8 +1,9 @@
 //👉 Write your tests below here:
 
-import decodeTheMessage from "./main";
+import { decodeTheMessage } from "./main";
 
 describe("Testing the decodeTheMessage function", () => {
+
     test("'mjqqt' should return 'hello'", () => {
         // ARRANGE
         const message = "mjqqt";
@@ -13,10 +14,58 @@ describe("Testing the decodeTheMessage function", () => {
         expect(actual).toBe(expected);
     });
 
-    test("'duwe cm hin jusch nby vcffm, sio ehiq?' should return 'jack is not paying the bills, you know?' ", () => {
+    test("`nyx'd lo kxqbi.` should return `don't be angry.`", () => {
+        const message  = `nyx'd lo kxqbi.`;
+        const expected = `don't be angry.`;
+        const actual = decodeTheMessage(message);
+        expect(actual).toBe(expected);
+    })
+
+    test("`vsj vycd rob qvkccoc` should return `liz lost her glasses`", () => {
+        const message = `vsj vycd rob qvkccoc`;
+        const expected = `liz lost her glasses`;
+        const actual = decodeTheMessage(message);
+        expect(actual).toBe(expected);
+    })
+
+    test("`x adkt hfjpih xc iwt bdgcxcv` should return `i love squats in the morning`", () => {
+        const message = `x adkt hfjpih xc iwt bdgcxcv`;
+        const expected = `i love squats in the morning`;
+        const actual = decodeTheMessage(message);
+        expect(actual).toBe(expected);
+    })
+
+    test("`pqrstuvwxyzabdcefghijklmno` should return `abcdefghijklmonpqrstuvwxyz`", () => {
+        const message = `pqrstuvwxyzabdcefghijklmno`;
+        const expected = `abcdefghijklmonpqrstuvwxyz`;
+        const actual = decodeTheMessage(message);
+        expect(actual).toBe(expected);
+    })
+    
+    test("'duwe cm hin juscha nby vcffm, sio ehiq?' should return 'jack is not paying the bills, you know?' ", () => {
         // ARRANGE
-        const message = "duwe cm hin jusch nby vcffm, sio ehiq?" ;
+        const message = "duwe cm hin juscha nby vcffm, sio ehiq?" ;
         const expected = "jack is not paying the bills, you know?";
+        // ACT
+        const actual = decodeTheMessage(message);
+        // ASSERT
+        expect(actual).toBe(expected);
+    });
+    
+    test("'roi, ry, vod'c qy!' should return 'hey, ho, let's go!'", () => {
+        // ARRANGE
+        const message = "roi, ry, vod'c qy!" ;
+        const expected = "hey, ho, let's go!";
+        // ACT
+        const actual = decodeTheMessage(message);
+        // ASSERT
+        expect(actual).toBe(expected);
+    });
+
+    test("'cn'm u nymn ch dymn, u wifiolzof voazymn' should return 'it's a test in jest, a colourful bugfest'", () => {
+        // ARRANGE
+        const message = "cn'm u nymn ch dymn, u wifiolzof voazymn"; 
+        const expected = "it's a test in jest, a colourful bugfest";
         // ACT
         const actual = decodeTheMessage(message);
         // ASSERT
@@ -53,35 +102,6 @@ describe("Testing the decodeTheMessage function", () => {
         expect(actual).toBe(expected);
     })
 
-    test("'roi, ry, vod'c qy!' should return 'hey, ho, let's go!'", () => {
-        // ARRANGE
-        const message = "roi, ry, vod'c qy!" ;
-        const expected = "hey, ho, let's go!";
-        // ACT
-        const actual = decodeTheMessage(message);
-        // ASSERT
-        expect(actual).toBe(expected);
-    });
-
-    test("'cn'm u nymn ch dymn, u wifiolzof vozymn' should return 'it's a test in jest, a colourful bugfest'", () => {
-        // ARRANGE
-        const message = "cn'm u nymn ch dymn, u wifiolzof vozymn"; 
-        const expected = "it's a test in jest, a colourful bugfest";
-        // ACT
-        const actual = decodeTheMessage(message);
-        // ASSERT
-        expect(actual).toBe(expected);
-    });
-
-    test("'mojylwufczlucfcmncwyrjcufcxiwciom' should return 'supercalifragilisticexpialidocious'", () => {
-        // ARRANGE
-        const message = "mojylwufczlucfcmncwyrjcufcxiwciom";
-        const expected = "supercalifragilisticexpialidocious";
-        // ACT
-        const actual = decodeTheMessage(message);
-        // ASSERT
-        expect(actual).toBe(expected);
-    });
 
     test("'12345____6789____)20[]#'/.' should return '12345____6789____)20[]#'/.'", () => {
         // ARRANGE
